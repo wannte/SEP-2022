e90d41e209b23b1ffc1c6a5171ef4b091656cad9
kunholee_98 <dlrjsgh750@naver.com>
2022-05-22 19:55:26 +0900

    add(routes): status_code 추가, 모든 수강 강의 삭제 API

--- backend/apps/src/routes/apis/v1/user/routes.py
+++ backend/apps/src/routes/apis/v1/user/routes.py
@@ -29,56 +29,61 @@ def check_user_exists(student_id: str, db: Session = Depends(get_db)):
 def sign_up(student_id: str = Body(...), major: str = Body(...), db: Session = Depends(get_db)):
     if UserService.get_user_by_student_id(student_id, db):
         return JSONResponse(content={'ok': False, 'message': '이미 존재하는 유저입니다.'}, status_code=400)
-    if UserService.create_user(User(student_id = student_id, major = major), db):
-        return {'ok': True}
-    return {'ok': False, 'message': '유저 생성에 실패하였습니다.'}
+    if not UserService.create_user(User(student_id = student_id, major = major), db):
+        return JSONResponse(content={'ok': False, 'message': '유저 생성에 실패하였습니다.'}, status_code=400)
+    return {'ok': True}
 
 @rt.put('/major')
 def change_major(major: str = Body(...) , user: User = Depends(get_user), db: Session = Depends(get_db)):
     user.major = major
-    if UserService.update_user(user, db):
-        return {'ok': True}
-    return {'ok': False, 'message': '전공 변경에 실패하였습니다.'}
+    if not UserService.update_user(user, db):
+        return JSONResponse(content={'ok': False, 'message': '전공 변경에 실패하였습니다.'}, status_code=400)
+    return {'ok': True}
 
 
 @rt.get('/lectures', description='해당 학기의 유저가 수강한 강의 목록')
 def get_user_lectures(year: str, semester: str, user: User = Depends(get_user), db: Session = Depends(get_db)):
     if not user:
-        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'})
+        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'}, status_code=401)
     return LectureService.get_all_lectures_by_year_semester_user_id(year, semester, user.id, db)
 
 @rt.post('/lectures/{lecture_id}')
 def add_lecture(lecture_id: int, user: User = Depends(get_user), db: Session = Depends(get_db)):
     if not user:
-        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'})
+        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'}, status_code=401)
     if LearnedService.get_learned_by_lecture_id_and_user_id(user.id, lecture_id, db):
         return JSONResponse(content={'ok': False, 'message': '이미 등록된 과목입니다.'}, status_code=400)
     learned = Learned(student_id = user.id, lecture_id = lecture_id)
     if not LearnedService.create_learned(learned, db):
-        return JSONResponse(content={'ok': False, 'message': 'leanred 생성을 실패했습니다.'})
+        return JSONResponse(content={'ok': False, 'message': 'leanred 생성을 실패했습니다.'}, status_code=400)
     return {'ok': True}
 
 @rt.delete('/lectures/{lecture_id}')
 def delete_lecture(lecture_id: int, user: User = Depends(get_user), db: Session = Depends(get_db)):
     if not user:
-        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'})
+        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'}, status_code=401)
     learned = LearnedService.get_learned_by_lecture_id_and_user_id(user.id, lecture_id, db)
     if not learned:
-        return JSONResponse(content={'ok': False, 'message': '등록되지 않은 과목입니다.'}, status_code=400)
+        return JSONResponse(content={'ok': False, 'message': '등록되지 않은 과목입니다.'}, status_code=404)
     if not LearnedService.delete_learned(learned, db):
-        return JSONResponse(content={'ok': False, 'message': 'leanred 삭제를 실패했습니다.'})
+        return JSONResponse(content={'ok': False, 'message': 'learned 삭제를 실패했습니다.'}, status_code=400)
     return {'ok': True}
 
+# year, semester를 고려할 것인가?
 @rt.delete('/lectures')
 def delete_all_lectures(user: User = Depends(get_user), db: Session = Depends(get_db)):
     if not user:
-        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'})
+        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'}, status_code=401)
+    learneds = LearnedService.get_all_learned_lecture_by_user_id(user.id, db)
+    if not LearnedService.delete_all_learned(learneds, db):
+        return JSONResponse(content={'ok': False, 'message': '수강 목록 전체 삭제에 실패했습니다.'}, status_code=400)
+    return {'ok': True}
     
 
 @rt.get('/credit/total')
 def count_all_credit(user: User = Depends(get_user), db: Session = Depends(get_db)):
     if not user:
-        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'})
+        return JSONResponse(content={'ok': False, 'message': '없는 유저입니다.'}, status_code=401)
     lectures = LectureService.get_all_lectures_by_user_id(user.id, db)
     total_credit = 0
     for lecture in lectures:
