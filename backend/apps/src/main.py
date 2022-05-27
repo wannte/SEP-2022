cb210bff60e40453005ad91faca95c6717eb475f
kunholee_98 <dlrjsgh750@naver.com>
2022-05-27 10:15:00 +0900

    add(main): CORS

--- backend/apps/src/main.py
+++ backend/apps/src/main.py
@@ -10,14 +10,6 @@ app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origin_regex='.*',
-    allow_credentials=True,
-    allow_methods=origins,
-    allow_headers=origins,
-)
-
 from apps.src.routes.apis.v1.user import routes as user_routes
 from apps.src.routes.apis.v1.lecture import routes as lecture_routes
 from apps.src.routes.apis.v1.data import routes as data_routes
@@ -25,3 +17,11 @@ from apps.src.routes.apis.v1.data import routes as data_routes
 app.include_router(user_routes.rt)
 app.include_router(lecture_routes.rt)
 app.include_router(data_routes.rt)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex='.*',
+    allow_credentials=True,
+    allow_methods=origins,
+    allow_headers=origins,
+)
