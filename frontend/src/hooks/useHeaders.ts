07e37b9e32a68dfdc10b87f8a4a40eb3cc31df77
kimjngyun <sanbada79@naver.com>
2022-06-02 22:40:41 +0900

    chore: 함수 사이 엔터

--- frontend/src/hooks/useHeaders.ts
+++ frontend/src/hooks/useHeaders.ts
@@ -4,18 +4,22 @@ import { useAppSelect } from "./useStore";
 export const useHeaders = () => {
   const sid = useAppSelect((select) => select.user.studentId);
   const hs = { "student-id": sid };
+
   const fetch = async (url: string) => {
     const response = await API.get(url, { headers: hs });
     return response;
   };
+
   const post = async (url: string, payload: any) => {
     const response = await API.post(url, payload, { headers: hs });
     return response;
   };
+
   const put = async (url: string, payload: any) => {
     const response = await API.put(url, payload, { headers: hs });
     return response;
   };
+
   const del = async (url: string) => {
     const response = await API.delete(url, { headers: hs });
     return response;
